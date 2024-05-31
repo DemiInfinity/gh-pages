182f92656cf4bead4b02681e989c689a89db68e4
Demi Taylor Nimmo <missdemitg@outlook.com>
2024-05-31 20:27:28 +0100

    updated config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/gh-pages' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/gh-pages' : '',
+  output: process.env.NODE_ENV === 'production' ? "export": '',
+  reactStrictMode: true,
   images: {
     domains: ['raw.githubusercontent.com'],
+    loader: 'akamai',
+    path: '',
   },
   publicRuntimeConfig: {
     basePath: process.env.NODE_ENV === 'production' ? '/gh-pages' : '',
@@ -11,5 +15,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-  

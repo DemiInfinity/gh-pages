9a9417c8bd8d35aa0081f2f97cb9789b46f57549
Demi Taylor Nimmo <missdemitg@outlook.com>
2024-05-31 17:35:38 +0100

    home page fix images

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export default function Home() {
   const isProduction = process.env.NODE_ENV === 'production';
-  const basePath = isProduction ? '/gh-pages' : '';
+  const basePath = isProduction ? '/gh-pages/' : '';
 
   return (
     <main className="w-screen h-screen relative">
       <div
         className="flex items-center w-full h-full bg-cover bg-center"
-        style={{ backgroundImage: `url(${basePath}/main-bg.webp)` }}
+        style={{ backgroundImage: `url(${basePath}main-bg.webp)` }}
       >
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">

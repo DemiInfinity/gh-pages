e5d81258333412d12c75ba0af0386ed00e6052f5
Demi Taylor Nimmo <missdemitg@outlook.com>
2024-05-31 16:15:45 +0100

    updated config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: "/gh-pages",
+    output: "export",
+    reactStrictMode: true,
+}
 
 module.exports = nextConfig

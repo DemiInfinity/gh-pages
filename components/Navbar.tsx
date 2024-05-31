7241526096f31ad8fbb54bfd285de7ff5350a940
Demi Taylor Nimmo <missdemitg@outlook.com>
2024-05-31 18:22:15 +0100

    Social Icon fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,19 +5,12 @@ import React from "react";
 
 const Navbar = () => {
   const isProduction = process.env.NODE_ENV === 'production';
-  const baseURL = isProduction ? '/gh-pages/' : '';
-
-  const handleImageClick = (url:any) => {
-    window.open(url, '_blank');
-  };
-
+  const baseURL = isProduction ? '/gh-pages' : '';
   return (
     <div className="fixed top-0 z-[40] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20">
       <div className="flex flex-row gap-3 items-center">
         <Link href="/">
-          <a>
-            <h1 className="text-white text-[25px] font-semibold">Demi Taylor Nimmo</h1>
-          </a>
+          <h1 className="text-white text-[25px] font-semibold">Demi Taylor Nimmo</h1>
         </Link>
       </div>
 
@@ -25,12 +18,10 @@ const Navbar = () => {
         {Socials.map((social) => (
           <Image
             key={social.name}
-            src={baseURL + social.src}
+            src={baseURL+social.src}
             alt={social.name}
             width={28}
             height={28}
-            onClick={() => handleImageClick(social.link)}
-            style={{ cursor: 'pointer' }}
           />
         ))}
       </div>

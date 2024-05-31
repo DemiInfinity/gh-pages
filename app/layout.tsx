3c8aeba80092d4f1f16b4c554e36dff2cb2f1be8
Demi Taylor Nimmo <missdemitg@outlook.com>
2024-06-01 00:12:15 +0100

    updated the metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,10 @@ import Navigation from "@/components/Navigation";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Demi Taylor Nimmo",
-  description: "Portfolio website with Tailwind CSS and Typescript.",
+  title: "Demi Taylor Nimmo - Professional Software Developer & Computer Scientist",
+  description: "Explore the portfolio of Demi Taylor Nimmo, a highly skilled software developer with a strong background in computer science. Specializing in full-stack development, Tailwind CSS, and TypeScript, Demi creates robust and innovative solutions. Discover projects, skills, and the journey of a passionate developer dedicated to pushing the boundaries of technology.",
 };
+
 
 export default function RootLayout({
   children,

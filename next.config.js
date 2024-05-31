4621bfa9d5e3852846c91fe2f1ca3626ba7d0ac6
Demi Taylor Nimmo <missdemitg@outlook.com>
2024-05-31 20:24:09 +0100

    updating files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/gh-pages' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/gh-pages' : '',
   images: {
     domains: ['raw.githubusercontent.com'],
   },

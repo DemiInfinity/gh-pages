44878648d571bfe6ac748e7412863c6777c59fe5
Demi Taylor Nimmo <missdemitg@outlook.com>
2024-09-20 14:39:02 +0100

    updating base path

--- app/utils/basePath.ts
+++ app/utils/basePath.ts
@@ -1,10 +1,5 @@
 import getConfig from 'next/config';
 
 export const getBasePath = (): string => {
-  if (typeof window === 'undefined') {
-    const { publicRuntimeConfig } = getConfig();
-    return publicRuntimeConfig.basePath || '';
-  } else {
     return '';
-  }
 };
